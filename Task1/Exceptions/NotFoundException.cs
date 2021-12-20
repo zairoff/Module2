efaec0c72ac3b54c221f5073fff860fb7c699759
Maruf <marufzoiroff@gmail.com>
2021-12-20 14:19:42 +0500

    implemented CategoryService. created custom exception

---
+++ Task1/Exceptions/NotFoundException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task1.Exceptions
+{
+    [Serializable]
+    public class NotFoundException : Exception
+    {
+        public NotFoundException()
+        {
+        }
+
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ Task1/Services/Contracts/IBaseService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task1.Services.Contracts
+{
+    public interface IBaseService<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(int id);
+    }
+}
